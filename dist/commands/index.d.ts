c09743357f005d471f56f439d4553b07fe01b73a
Fernando Ridam <30510101+FernandoRidam@users.noreply.github.com>
2022-07-27 03:24:35 -0300

    Delete dist directory

--- dist/commands/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type Type = 'number' | 'string' | 'Date';
-export interface Column {
-    type: Type;
-}
-export interface Table {
-    [key: string]: Column;
-}
-export declare const select: <T>(columns: T) => void;
-export declare const createSchema: (table: Table) => void;
